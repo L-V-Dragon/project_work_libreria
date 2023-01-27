5fbb86ef47c06d71747f4c34cef50ab9de3e74f2
L-V-Dragon <viotti37@gmail.com>
2023-01-27 10:28:58 +0100

    Add project files.

---
+++ project_work_libreria/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace project_work_libreria.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
