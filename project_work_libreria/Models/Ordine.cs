3cfcf69c462a0dafa0366841e1c34c7587b71af8
VolpinAndrea <andvolpin19@gmail.com>
2023-01-27 14:54:37 +0100

    DB ordine e fornitore

---
+++ project_work_libreria/Models/Ordine.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace project_work_libreria.Models
+{
+    public class Ordine
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Questo campo è obbligatorio")]
+        public int Quantita { get; set; }
+
+        [Required]
+        public DateTime Data { get; set; }
+
+        //Chiavi per relazioni con utente e libro
+        public List<User>? nUtenti { get; set; }
+        public List<Libro>? nLibri { get; set; }
+
+        //Chiave per relazione 1 a N con fornitore
+        public int FornitoreId { get; set; }
+        public Fornitore? Fornitore { get; set; }
+
+        //Costruttori
+        public Ordine() { }
+        public Ordine(int quantita)
+        {
+            Quantita = quantita;
+            Data = DateTime.Now;  //funziona?
+        }
+    }
+} 
