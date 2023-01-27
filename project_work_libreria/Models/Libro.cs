514f7f78eacba4e7b007a1f33d736f6f8ba78264
VolpinAndrea <andvolpin19@gmail.com>
2023-01-27 15:41:51 +0100

    BD Libro

--- project_work_libreria/Models/Libro.cs
+++ project_work_libreria/Models/Libro.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using project_work_libreria.CustomValidation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace project_work_libreria.Models {
     public class Libro {
@@ -6,14 +9,27 @@ namespace project_work_libreria.Models {
         [Key]
         public int Id { get; set; }
 
+        [Required]
+        public string Isbm { get; set; }
+
+        [Required]
         public string Titolo { get; set; }
 
+        [Required]
         public string Trama { get; set; }
 
-        public string Foto { get; set; }
-            
-        public int Prezzo { get; set; }
+        [Required]
+        public string Autore { get; set; }
 
+        [Required(ErrorMessage = "Questo campo è obbligatorio")]
+        [Url(ErrorMessage = "Hai inserito un link non valido")]
+        [ImageUrlValidation]
+        public string Foto { get; set; }
+
+        [Required]
+        public double Prezzo { get; set; }
+
+        [Required]
         public int? Quantita { get; set; }
 
         public int? Like { get; set; }
@@ -22,8 +38,6 @@ namespace project_work_libreria.Models {
         public Genere? Genere { get; set; }
 
         public Libro() { }
-
-
 
     }
 }
