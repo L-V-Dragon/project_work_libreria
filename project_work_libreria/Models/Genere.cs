93916c0f99bd0005662145b0b77ca73496d7f9ea
L-V-Dragon <viotti37@gmail.com>
2023-01-27 12:47:30 +0100

    Modellli Genere e Libro fatti

---
+++ project_work_libreria/Models/Genere.cs
@@ -0,0 +1,11 @@
+﻿namespace project_work_libreria.Models
+{
+    public class Genere
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        List<Libro> Libri  { get; set; }
+    }
+}
--- project_work_libreria/Models/Libro.cs
+++ project_work_libreria/Models/Libro.cs
@@ -6,6 +6,21 @@ namespace project_work_libreria.Models {
         [Key]
         public int Id { get; set; }
 
+        public string Titolo { get; set; }
+
+        public string Trama { get; set; }
+
+        public string Foto { get; set; }
+            
+        public int Prezzo { get; set; }
+
+        public int? Quantita { get; set; }
+
+        public int? Like { get; set; }
+
+        public int GenereId { get; set; }
+        public Genere Genere { get; set; }
+
         public Libro() { }
 
 
