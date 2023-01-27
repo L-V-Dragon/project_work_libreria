0832128935f83e65a3de6c11e0f0460a5cf3e9f6
Pedra96 <darkraidialga@yahoo.it>
2023-01-27 22:21:54 +0100

    cambiato dbContext

--- project_work_libreria/Database/LibreriaContext.cs
+++ project_work_libreria/Database/LibreriaContext.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using project_work_libreria.Models;
 
 namespace project_work_libreria.Database {
-    public class LibreriaContext: DbContext {
+    public class LibreriaContext: IdentityDbContext<IdentityUser> {
 
         public DbSet<Libro> Libri { get; set; }
 
