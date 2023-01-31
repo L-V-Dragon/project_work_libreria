cb51b72f8cffe7dac9491937fdc534277a4bd0ca
Pedra96 <darkraidialga@yahoo.it>
2023-01-31 02:38:41 +0100

    creato metodo api per amministatore e cliente sia index che dettagli e aggiunta la possibilità di usare la barra di ricerca per filtrare i libri per titolo,descrizione,genere e isbn.

--- project_work_libreria/Controllers/ClienteController.cs
+++ project_work_libreria/Controllers/ClienteController.cs
@@ -7,7 +7,7 @@ namespace project_work_libreria.Controllers {
     public class ClienteController : Controller {
         public IActionResult Index() {
             using LibreriaContext db = new();
-            List<Libro> listaLibri = db.Libri.OrderBy(x => x.Quantita).ToList<Libro>();
+            List<Libro> listaLibri = db.Libri.OrderBy(x => x.Titolo).Include(x=> x.Genere).ToList<Libro>();
 
             return View(listaLibri);
         }
@@ -28,8 +28,23 @@ namespace project_work_libreria.Controllers {
 
                 return NotFound("Il libro con l'id cercato non esiste!");
 
+            } 
+        }
+        public IActionResult DettagliApi(int id) {
+            using (LibreriaContext db = new LibreriaContext()) {
+
+                Libro libroTrovato = db.Libri
+                    .Where(SingoloLibroNelDb => SingoloLibroNelDb.Id == id)
+                    .Include(Libro => Libro.Genere)
+                    .FirstOrDefault();
+
+                if (libroTrovato != null) {
+                    return View(libroTrovato);
+                }
+
+                return NotFound("Il libro con l'id cercato non esiste!");
+
             }
-          
         }
     }
 }
